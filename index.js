c323adb909a5f1bc314f888ea034858dc492ad30
Stephano <43631068+stephanyes@users.noreply.github.com>
2019-03-29 20:10:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function includesText(texto1,texto2){
+// function includesText(texto1,texto2){
 /*    var texto1 = texto1.toUpperCase();
     var texto2 = texto2.toUpperCase();
 
     Sin el var se vuelve global
  */
- if (typeof texto1 === 'string' && typeof texto2 === 'string') {
-    if (texto2.toUpperCase().indexOf(texto1.toUpperCase()) === -1){
-        return false;
-        } else {
-            return true;
-            }
-          }
-
-
-          console.log(includesText('Pa', 'patricia'));
-
-}
+//  if (typeof texto1 === 'string' && typeof texto2 === 'string') {
+//     if (texto2.toUpperCase().indexOf(texto1.toUpperCase()) === -1){
+//         return false;
+//         } else {
+//             return true;
+//             }
+//           }
+//
+//
+//           console.log(includesText('Pa', 'patricia'));
+//
+// }
 
           function includesText (text, baseText) {
               if (typeof text === 'string' && typeof baseText === 'string'){
-                  if(text.toUpperCase().indexOf(baseText.toUpperCase()) !== -1){
+                  if(baseText.toUpperCase().indexOf(text.toUpperCase()) !== -1){
                       return true;
                   } else {
                       return false;
