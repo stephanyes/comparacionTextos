4e46be260fad9bd7249864687d231e0c1c2e62d2
Stephano <43631068+stephanyes@users.noreply.github.com>
2019-03-29 20:07:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-  function includesText(texto1,texto2){
-  /*     texto1 = texto1.toUpperCase();
-      texto2 = texto2.toUpperCase();
-   */
-  if (texto2.toUpperCase().indexOf(texto1.toUpperCase()) === -1){
-          return false;
-      } else {
-          return true;
-    }
+function includesText(texto1,texto2){
+/*    var texto1 = texto1.toUpperCase();
+    var texto2 = texto2.toUpperCase();
+
+    Sin el var se vuelve global
+ */
+ if (typeof texto1 === 'string' && typeof texto2 === 'string') {
+    if (texto2.toUpperCase().indexOf(texto1.toUpperCase()) === -1){
+        return false;
+        } else {
+            return true;
+            }
+          }
+
+
+          console.log(includesText('Pa', 'patricia'));
+
 }
-console.log(includesText('Pa', 'Parmeggiano'));
+
+          function includesText (text, baseText) {
+              if (typeof text === 'string' && typeof baseText === 'string'){
+                  if(text.toUpperCase().indexOf(baseText.toUpperCase()) !== -1){
+                      return true;
+                  } else {
+                      return false;
+                  }
+              } else {
+                  return false;
+              }
+          }
